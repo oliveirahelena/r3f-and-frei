09f2a61d59a4696e40dc31f8a47c8ef7bebcd608
Helena Oliveira <helena@informach.com.br>
2026-02-14 14:28:41 -0300

    add drei

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,3 +1,68 @@
+import { MeshReflectorMaterial, Float, Text, Html, OrbitControls, TransformControls, PivotControls } from "@react-three/drei"
+import { useRef } from "react"
+
 export default function Experience() {
-    return <></>
+    const cube = useRef()
+    const sphere = useRef()
+
+    return <>
+        <OrbitControls makeDefault />
+
+        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
+
+        <PivotControls anchor={[0, 0, 0]}
+            depthTest={false}
+            lineWidth={4}
+            axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+            scale={2}>
+            <mesh ref={sphere} position-x={- 2}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+                <Html
+                    wrapperClass="label"
+                    position={[1, 1, 0]}
+                    center
+                    distanceFactor={8}
+                    occlude={[cube, sphere]}
+                >Isso é uma esfera!</Html>
+            </mesh>
+        </PivotControls>
+
+        <mesh ref={cube} position-x={2} scale={1.5}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <TransformControls object={cube} />
+
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
+            <planeGeometry />
+            {/* <meshStandardMaterial color="greenyellow" /> */}
+            <MeshReflectorMaterial
+                resolution={512}
+                blur={[1000, 1000]}
+                mixBlur={1}
+                mirror={0.5}
+                color="greenyellow"
+            />
+        </mesh>
+
+        <Float
+            speed={5}
+            floatIntensity={2}
+        >
+            <Text
+                font="./bangers-v20-latin-regular.woff"
+                fontSize={1}
+                color="salmon"
+                position-y={2}
+                maxWidth={2}
+                textAlign="center"
+            >
+                I LOVE R3F
+            </Text>
+        </Float>
+
+    </>
 }
